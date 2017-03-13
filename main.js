754d9f6a7b5313c4c6cb7369bc07df34a32cdabe
Alex <alex.kotlov@de.ibm.com>
2017-03-13 16:54:15 +0100

    commit

--- main.js
+++ main.js
@@ -1,26 +1,56 @@
-function initMap() {
-        var carImage = 'https://raw.githubusercontent.com/alexkvibm/hackoit2017/master/images/car.png';
-        var carPos = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: carPos
-        });
-        var car = new google.maps.Marker({
-          position: carPos,
-          icon: carImage,
-          title: 'my car',
-          map: map
-        });
-        
-        var gasStationImage = 'https://raw.githubusercontent.com/alexkvibm/hackoit2017/master/images/station.png';
-        var gasStationPos = {lat: -23.363, lng: 133.044};
-        var gasStation = new google.maps.Marker({
-          position: gasStationPos,
-          icon: gasStationImage,
-          title: 'gas station AAA',
-          map: map
-        });
-}
+
+
+var map;
+var markers = [];
+
+
+  function initMap() {
+            var carImage = 'https://raw.githubusercontent.com/alexkvibm/hackoit2017/master/images/car.png';
+            var carPos = {lat: -25.363, lng: 131.044};
+
+            addMarker(carPos, carImage, 'My Car');
+            
+            var gasStationImage = 'https://raw.githubusercontent.com/alexkvibm/hackoit2017/master/images/station.png';
+            var gasStationPos = {lat: -23.363, lng: 133.044};
+          
+            addMarker(gasStationPos, gasStationImage, 'Gas gasStation A');
+  }
+
+
+  // Adds a marker to the map and push to the array.
+  function addMarker(location, image, caption) {
+    var marker = new google.maps.Marker({
+      position: location,
+      icon: image,
+      title: caption,
+      map: map
+    });
+    markers.push(marker);
+  }
+
+  // Sets the map on all markers in the array.
+  function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
+
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
+    setMapOnAll(null);
+  }
+
+  // Shows any markers currently in the array.
+  function showMarkers() {
+    setMapOnAll(map);
+  }
+
+  // Deletes all markers in the array by removing references to them.
+  function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+  }
+
 
 function showTelemetry(){
    var speed = 100; 
